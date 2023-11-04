12e31f3c93cb5d82aabb689d73bc022aaa6ba2f7
Albattal <zaldaas@csu.fullerton.edu>
2023-11-03 20:54:29 -0700

    Implemented MainActivity.java and RegistrationActivity.java and incorporated firebase connections in gradle files.

--- app/src/main/java/com/example/sportspot/MainActivity.java
+++ app/src/main/java/com/example/sportspot/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.sportspot;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button navigateToRegisterButton = findViewById(R.id.navigateToRegisterButton);
+
+        navigateToRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sportspot/RegistrationActivity.java
+++ app/src/main/java/com/example/sportspot/RegistrationActivity.java
@@ -1,14 +1,68 @@
 package com.example.sportspot;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    // Declare Firebase Auth and UI elements
+    private FirebaseAuth mAuth;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Reference UI elements
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        signUpButton = findViewById(R.id.signUpButton);
+
+        // Set OnClickListener for the Sign Up button
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        // Basic validation
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(RegistrationActivity.this, "Email and Password required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Firebase registration
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegistrationActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegistrationActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
