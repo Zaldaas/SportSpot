059b27ec6f0b9fadde6dccff89de27209732b000
ZootZoot-25 <49415426+ZootZoot-25@users.noreply.github.com>
2023-11-07 03:34:59 -0800

    added firebase connectivity

--- app/src/main/java/com/example/sportspot/MainActivity.java
+++ app/src/main/java/com/example/sportspot/MainActivity.java
@@ -9,15 +9,12 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //slowly swipe from the left to bring up navigation menu
 
-
-        Button navigateToRegisterButton = findViewById(R.id.navigateToRegisterButton);
-
+        /*
         navigateToRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +22,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        */
     }
 }
