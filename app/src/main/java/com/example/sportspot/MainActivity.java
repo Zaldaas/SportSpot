a0d65f624586b054ac96fe267be30d98dd123c95
ZootZoot-25 <49415426+ZootZoot-25@users.noreply.github.com>
2023-11-07 03:34:59 -0800

    Added a navigation menu that can be pulled slowly from the left side

--- app/src/main/java/com/example/sportspot/MainActivity.java
+++ app/src/main/java/com/example/sportspot/MainActivity.java
@@ -1,21 +1,99 @@
 package com.example.sportspot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postList;
+    private ImageButton AddNewPostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //slowly swipe from the left to bring up navigation menu
+        /*
+        navigateToRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                
+            }
+        });
 
-        }
+        */
+
+        //create objects for functions
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        // AddNewPostButton = (ImageButton) findViewById(R.id.add_new_post_button);   for adding posts
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
 
 
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // calls UserMenuSelector to select a case when a button from the side menu is clicked
+                //false is a neutral state
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+        /*
+        AddNewPostButton.setOnClickListener(new View.OnClickListener() {   add post button not ready
+            @Override
+            public void onClick(View v) {
+                SendUserToPostActivity();
+            }
+        });
+
+         */
     }
 
+    /*
+    private void SendUserToPostActivity() {
+        Intent addNewPostIntent = new Intent(MainActivity.this, PostActivity.class);
+        startActivity(addNewPostIntent);
+    }
+
+     */
+
+    private void UserMenuSelector(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.nav_profile) {
+            Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.nav_add_new_post) {
+            Toast.makeText(this, "add new post", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.nav_home) {
+            Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.nav_friends) {
+            Toast.makeText(this, "Friend List", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.nav_find_friends) {
+            Toast.makeText(this, "Find Friends", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.nav_messages) {
+            Toast.makeText(this, "Messages", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.nav_settings) {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.nav_logout) {
+            Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
