00a28abdebcdb32d8aaea18873f48557a4723eb0
ZootZoot-25 <49415426+ZootZoot-25@users.noreply.github.com>
2023-11-21 01:09:05 -0800

    Added multiple files to create a working post activity, requires login and regestration information to work

--- app/src/main/java/com/example/sportspot/MainActivity.java
+++ app/src/main/java/com/example/sportspot/MainActivity.java
@@ -1,49 +1,88 @@
 package com.example.sportspot;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     private RecyclerView postList;
+    private Toolbar mToolbar;
+
+
     private ImageButton AddNewPostButton;
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef, PostsRef;
+
+    String currentUserID;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //slowly swipe from the left to bring up navigation menu
-        /*
-        navigateToRegisterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(intent);
-                
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        PostsRef = FirebaseDatabase.getInstance().getReference().child("Posts");
 
-        */
+        //functions for the action bar submenu
+        mToolbar = (Toolbar) findViewById(R.id.main_page);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
 
-        //create objects for functions
+        //new post button function
+        AddNewPostButton = (ImageButton) findViewById(R.id.add_new_post_button);
+
+        //functions for the drawer layout
         drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         navigationView = (NavigationView) findViewById(R.id.navigation_view);
-        // AddNewPostButton = (ImageButton) findViewById(R.id.add_new_post_button);   for adding posts
+
+        postList = (RecyclerView) findViewById(R.id.all_users_post_list);
+        postList.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        postList.setLayoutManager(linearLayoutManager);
+
+
         View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
 
 
@@ -57,31 +96,115 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-        /*
-        AddNewPostButton.setOnClickListener(new View.OnClickListener() {   add post button not ready
+
+        AddNewPostButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 SendUserToPostActivity();
             }
         });
-
-         */
+        DisplayAllUsersPosts();
     }
 
-    /*
+    private void DisplayAllUsersPosts() {
+        FirebaseRecyclerOptions<Posts> options = new FirebaseRecyclerOptions.Builder<Posts>()
+                .setQuery(PostsRef, Posts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Posts, PostsViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Posts, PostsViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull PostsViewHolder holder, int position, @NonNull Posts model) {
+                        holder.setFullname(model.getFullname());
+                        holder.setTime(model.getTime());
+                        holder.setDate(model.getDate());
+                        holder.setDescription(model.getDescription());
+                        holder.setProfileimage(getApplicationContext(), model.getProfileimage());
+                        holder.setPostimage(getApplicationContext(), model.getPostimage());
+                    }
+
+                    @NonNull
+                    @Override
+                    public PostsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.all_posts_layout, parent, false);
+                        return new PostsViewHolder(view);
+                    }
+                };
+        postList.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
+    }
+
+    public static class PostsViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public PostsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+        public void setFullname(String fullname)
+        {
+            TextView username = (TextView) mView.findViewById(R.id.post_user_name);
+            username.setText(fullname);
+        }
+
+        public void setProfileimage(Context ctx, String profileimage)
+        {
+            CircleImageView image = (CircleImageView) mView.findViewById(R.id.post_profile_image);
+            Picasso.get().load(profileimage).into(image);
+        }
+
+        public void setTime(String time)
+        {
+            TextView PostTime = (TextView) mView.findViewById(R.id.post_time);
+            PostTime.setText("    " + time);
+        }
+
+        public void setDate(String date)
+        {
+            TextView PostDate = (TextView) mView.findViewById(R.id.post_date);
+            PostDate.setText("    " + date);
+        }
+
+        public void setDescription(String description)
+        {
+            TextView PostDescription = (TextView) mView.findViewById(R.id.post_description);
+            PostDescription.setText(description);
+        }
+
+        public void setPostimage(Context ctx1,  String postimage)
+        {
+            ImageView PostImage = (ImageView) mView.findViewById(R.id.post_image);
+            Picasso.get().load(postimage).into(PostImage);
+        }
+    }
+
     private void SendUserToPostActivity() {
         Intent addNewPostIntent = new Intent(MainActivity.this, PostActivity.class);
         startActivity(addNewPostIntent);
     }
 
-     */
+    //syncs the actionbar with the item menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
+
+    //creates a toast when a box is clicked from the item menu
     private void UserMenuSelector(MenuItem item) {
         int itemId = item.getItemId();
         if (itemId == R.id.nav_profile) {
             Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.nav_add_new_post) {
-            Toast.makeText(this, "add new post", Toast.LENGTH_SHORT).show();
+            SendUserToPostActivity();
+            //Toast.makeText(this, "add new post", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.nav_home) {
             Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.nav_friends) {
