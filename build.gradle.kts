95a788bd6537b32d846c4f00ae409f9c6ae93ffd
Zaldaas <zaldaas@csu.fullerton.edu>
2025-01-14 11:48:10 -0800

    Gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,15 @@
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.2")
     }
     repositories {
         google()
         mavenCentral()
-        jcenter()
     }
 }
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.android.application") version "8.8.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
